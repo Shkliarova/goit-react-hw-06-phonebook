e65754def9658524719db9bdd5e1a429b15c1467
Shkliarova <shklyarovaalisa@gmail.com>
2023-12-13 14:39:52 +0200

    complete redux persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import { GlobalStyle } from 'components/GlobalStyled';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
     <GlobalStyle/>
   </React.StrictMode>
 );
